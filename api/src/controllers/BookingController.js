2820ed94c24ebe8591a178549df961640eba0985
Jeferson <Carvalho>
2020-01-17 15:46:23 -0300

    cadastro e listagem dos spots

---
+++ api/src/controllers/BookingController.js
@@ -0,0 +1,6 @@
+module.exports = {
+    async store(req, res){
+        const { user_id } = req.headers;
+        const { spot_id } = req.params;
+    }
+};
--- api/src/routes.js
+++ api/src/routes.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const SessionController = require('./controllers/SessionController');
+const express = require("express");
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
+
+const SessionController = require("./controllers/SessionController");
+const SpotController = require("./controllers/SpotController");
+const DashboardController = require("./controllers/DashboardController");
+const BookingController = require("./controllers/BookingController");
+
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
-routes.post('/sessions', SessionController.store);
+routes.post("/sessions", SessionController.store);
+
+routes.get("/spots", SpotController.index);
+
+routes.post("/spots", upload.single("thumbnail"), SpotController.store);
+
+routes.get("/dashboard", DashboardController.show);
+
+routes.get("/spots/:spot_id/bookings", BookingController.store);
 
 //Exportando as routas para aplicacao
 module.exports = routes;
