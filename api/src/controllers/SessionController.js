3c73df248ca9895100f3d123f8d223d05ef2b0fb
Jeferson Carvalho <jeferson.carvalho@moneyex.io>
2020-01-18 16:28:45 -0300

    Componente de Login, redirecionamento para a tela de dashboard, chamada para API, instalacao do Axios

--- api/src/controllers/SessionController.js
+++ api/src/controllers/SessionController.js
@@ -3,25 +3,17 @@
 // store - criar uma sessão
 // update - Atualizar uma sessão
 // destroy - Deletar uma sessão
-const User = require('../models/User');
+const User = require("../models/User");
 
 module.exports = {
- async store(req, res){
+  async store(req, res) {
     const { email } = req.body;
 
     let user = await User.findOne({ email });
 
-    if(!user){
-      user = await User.create({ email });       
-    }else {
-      const resp = {
-         id: user._id,
-         email: user.email,
-         mgs: 'E-mail já cadastrado'
-      }
-      return res.json(resp);
+    if (!user) {
+      user = await User.create({ email });
     }
-
     return res.json(user);
- }
+  }
 };
--- api/src/server.js
+++ api/src/server.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const cors = require("cors");
 const port = 3333;
-const app = express()
+const app = express();
 
 // Conexao com o banco de dados mongodb
-mongoose.connect('mongodb+srv://jeff:jeff@cluster0-q87at.mongodb.net/aircnc?retryWrites=true&w=majority', {
+mongoose.connect(
+  "mongodb+srv://jeff:jeff@cluster0-q87at.mongodb.net/aircnc?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
+    useUnifiedTopology: true
+  }
+);
 
 // GET, POST, PUTT, DELETE
 // req.query = Acessar query params(para filtro)
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (criação e edição)
 
+app.use(cors());
 // Para express aceitar o formato de JSON
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port port!`));
