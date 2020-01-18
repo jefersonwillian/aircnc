68117080346cbdb79ca2ad717a447216d770c530
Jeferson Carvalho <jeferson.carvalho@moneyex.io>
2020-01-18 19:58:24 -0300

    tela para cadastrar um novo spot

--- site/src/pages/New/index.js
+++ site/src/pages/New/index.js
@@ -1,5 +1,89 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import api from "../../services/api";
 
-export default function New() {
-  return <div />;
+import camera from "../../assets/camera.svg";
+
+import "./styles.css";
+
+export default function New({ history }) {
+  const [thumbnail, setThumbnail] = useState(null);
+
+  const [company, setCompany] = useState("");
+  const [techs, setTechs] = useState("");
+  const [price, setPrice] = useState("");
+
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail]);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const data = new FormData();
+      const user_id = localStorage.getItem("user");
+
+      data.append("thumbnail", thumbnail);
+      data.append("company", company);
+      data.append("techs", techs);
+      data.append("price", price);
+
+      const response = await api.post("/spots", data, {
+        headers: { user_id }
+      });
+
+      history.push("/dashboard");
+    } catch (error) {
+      console.log("TCL: handleSubmit -> error", error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label
+        id="thumbnail"
+        style={{ backgroundImage: `url(${preview})` }}
+        className={thumbnail ? "has-thumbnail" : ""}
+      >
+        <input
+          type="file"
+          accept="image/*"
+          onChange={event => setThumbnail(event.target.files[0])}
+        />
+        <img src={camera} alt="Select img" />
+      </label>
+
+      <label htmlFor="company">EMPRESA *</label>
+      <input
+        id="company"
+        placeholder="Sua empresa incrível"
+        value={company}
+        onChange={event => setCompany(event.target.value)}
+      />
+
+      <label htmlFor="techs">
+        TECNOLOGIAS * <span>(separadas por vírgula)</span>
+      </label>
+      <input
+        id="techs"
+        placeholder="Sua empresa incrível"
+        value={techs}
+        onChange={event => setTechs(event.target.value)}
+      />
+
+      <label htmlFor="price">
+        VALOR DA DIÁRIA * <span>(em branco para GRATUITO)</span>
+      </label>
+      <input
+        id="price"
+        placeholder="Sua empresa incrível"
+        value={price}
+        onChange={event => setPrice(event.target.value)}
+      />
+
+      <button type="submit" className="btn">
+        Cadastrar
+      </button>
+    </form>
+  );
 }
